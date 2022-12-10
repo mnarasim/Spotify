98743c22a951c84e6ca2454ce9e6889c23e59880
mnarasim <Mani.Narasimhan@gmail.com>
2022-12-10 15:41:08 -0800

    recording files added

---
+++ utils/false.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 13:15:21 2022
+
+@author: mani
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 28 12:55:49 2022
+
+@author: mani
+"""
+
+import sounddevice as sd
+
+import wavio as wv
+import librosa
+import numpy as np
+import math
+import soundfile
+# Sampling frequency
+freq = 44100
+
+# Recording duration
+duration = 4
+
+start = 0
+end = 35
+name = 'false'
+
+
+for i in range(start, end, 1):
+    print('start')
+    recording = sd.rec(int(duration * freq), samplerate=freq, channels=1)
+
+    # Record audio for the given number of seconds
+    sd.wait()
+
+    wv.write(name+str(i)+".wav", recording, freq, sampwidth=2)
+    print('end')
+    print(' ')
+
+'''
+for i in range(start, end, 1):
+    signal, sr = librosa.load(name+str(i)+".wav", sr=16000)
+    RMS=math.sqrt(np.mean(signal**2))
+    STD_n= 0.1
+    noise=np.random.normal(0, STD_n, signal.shape[0])
+    signal_noise = signal + noise
+    soundfile.write(name + 'noise' +str(i)+'.wav',signal_noise,16100)
+'''
